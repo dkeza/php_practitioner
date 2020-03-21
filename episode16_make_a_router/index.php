637bbbb53ca28777d6bfbca0b9df7243b2e8da72
keza <dk@citadel.rs>
2020-03-21 19:13:07 +0100

    Add about culture page

--- episode16_make_a_router/index.php
+++ episode16_make_a_router/index.php
@@ -3,5 +3,12 @@
 $database = require($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "core/bootstrap.php");
 require($rootDir . "core/task.php");
 
+$routes = [
+  '' => 'controllers/index.php',
+  'about' => 'controllers/about.php',
+  'about/culture' => 'controllers/about.culture.php',
+  'contact' => 'controllers/contact.php'
+];
+
 
 require($rootDir . "controllers/index.php");
