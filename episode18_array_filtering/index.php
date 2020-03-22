140ac4858f1ba90b1770264f5a614d4f8d953b62
keza <dk@citadel.rs>
2020-03-22 12:31:04 +0100

    Array filter example

--- episode18_array_filtering/index.php
+++ episode18_array_filtering/index.php
@@ -1,5 +1,32 @@
 <?php
 
+class Post {
+
+    public $title;
+    public $published;
+
+    public function __construct($title, $published)
+    {
+        $this->title = $title;
+        $this->published = $published;
+    }
+}
+
+$posts = [
+    new Post("First post", true),
+    new Post("Second post", true),
+    new Post("Third post", true),
+    new Post("Forth post", false)
+];
+
+$unpublishedPosts = array_filter($posts, function($post) {
+    return !$post->published;
+});
+
+var_dump($unpublishedPosts);
+
+die();
+
 require("core/bootstrap.php");
 require("core/task.php");
 
