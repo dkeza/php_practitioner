d124e8e3a9bb0c001ff703b749b684ab5d2f8cbc
keza <dk@citadel.rs>
2020-03-22 11:50:22 +0100

    Add nav bar partial

---
+++ episode17_dry_up_your_views/views/index.view.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=1920, initial-scale=1.0">
+    <title>To Do list</title>
+</head>
+<body>
+
+    <?php require('partials/nav.php'); ?>
+
+    <h1>My Tasks</h1>
+
+    <ul>
+        <?php foreach($tasks as $task): ?>
+            <li>
+                <strong>Title: </strong>
+
+                <?php if ($task->completed): ?>
+                    <span style="text-decoration: line-through;">
+                <?php endif ?>
+
+                <?=$task->description;?>
+
+                <?php if ($task->completed): ?>
+                    </span>
+                <?php endif ?>
+
+            </li>
+        <?php endforeach ?>
+    </ul>
+
+</body>
+</html>
